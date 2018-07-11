512a1b10189b816120938b70d99cfb069aa96252
Philonenko Andrey <a.philonenko@tap.company>
2018-07-11 12:29:12 +0300

    Modified CardBrand, added new systems, update for gson parse.

--- library/src/main/java/company/tap/tapcardvalidator_android/CardBrand.java
+++ library/src/main/java/company/tap/tapcardvalidator_android/CardBrand.java
@@ -1,41 +1,42 @@
 package company.tap.tapcardvalidator_android;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * TapCardValidator
- *
+ * <p>
  * Copyright © 2018 Tap Payments. All rights reserved.
  */
 
 // Card Brand.
 public enum CardBrand {
-    aiywaLoyalty ("Aiywa Loyalty"),
-    americanExpress ("AMEX"),
-    cardGuard ("CARDGUARD"),
-    cbk ("CBK"),
-    dankort ("DANKORT"),
-    discover ("DISCOVER"),
-    dinersClub ("DINERS"),
-    instaPayment ("INSTAPAY"),
-    interPayment ("INTERPAY"),
-    jcb ("JCB"),
-    knet ("KNET"),
-    maestro ("MAESTRO"),
-    masterCard ("MASTERCARD"),
-    nspkMir ("NSPK"),
-    tap ("TAP"),
-    uatp ("UATP"),
-    unionPay ("UNIONPAY"),
-    verve ("VERVE"),
-    visa ("VISA"),
-    viva ("Viva PAY"),
-    wataniya ("Wataniya PAY"),
-    zain ("Zain PAY"),
 
-    unknown("");
-
-    private String value;
-
-    CardBrand(String value) {
-        this.value = value;
-    }
+    @SerializedName("aiywaLoyalty") aiywaLoyalty,
+    @SerializedName(value = "AMEX", alternate = {"AMERICAN_EXPRESS"}) americanExpress,
+    @SerializedName("BENEFIT") benefit,
+    @SerializedName("CARDGUARD") cardGuard,
+    @SerializedName("CBK") cbk,
+    @SerializedName("DANKORT") dankort,
+    @SerializedName("DISCOVER") discover,
+    @SerializedName(value = "DINERS_CLUB", alternate = {"DINERS"}) dinersClub,
+    @SerializedName("FAWRY") fawry,
+    @SerializedName("INSTAPAY") instaPayment,
+    @SerializedName("INTERPAY") interPayment,
+    @SerializedName("JCB") jcb,
+    @SerializedName("KNET") knet,
+    @SerializedName("MADA") mada,
+    @SerializedName("MAESTRO") maestro,
+    @SerializedName("MASTERCARD") masterCard,
+    @SerializedName("NAPS") naps,
+    @SerializedName("NSPK") nspkMir,
+    @SerializedName("SADAD_ACCOUNT") sadad,
+    @SerializedName("TAP") tap,
+    @SerializedName("UATP") uatp,
+    @SerializedName(value = "UNION_PAY", alternate = {"UNIONPAY"}) unionPay,
+    @SerializedName("VERVE") verve,
+    @SerializedName("VISA") visa,
+    @SerializedName("Viva PAY") viva,
+    @SerializedName("Wataniya PAY") wataniya,
+    @SerializedName("Zain PAY") zain,
+    @SerializedName("") unknown
 }
