6fce41e7e0c707e869016e5442dd9b1fa7d83613
AhlaamK-tap <57221514+AhlaamK-tap@users.noreply.github.com>
2024-05-09 17:06:22 +0300

    commit updte

--- library/src/main/java/company/tap/tapcardvalidator_android/CardValidator.java
+++ library/src/main/java/company/tap/tapcardvalidator_android/CardValidator.java
@@ -78,7 +78,7 @@ public class CardValidator {
     }
 
 
-    public int getCardLength(CardBrand cardBrand){
+    public  static int getCardLength(CardBrand cardBrand){
         List<CardBINRange> cardBINRanges = CardBINRange.ranges(cardBrand);
         if(cardBINRanges.isEmpty()){
             return 0;
